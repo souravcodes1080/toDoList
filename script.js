98334696b953aef1a1064ec200dabb14e1f2a515
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2023-06-26 10:22:38 +0530

    Add files via upload
    all files added

---
+++ script.js
@@ -0,0 +1,16 @@
+var input = document.getElementById('input');
+var add = document.getElementById('add');
+var list = document.getElementById('list');
+
+add.addEventListener('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.innerText = input.value;
+    list.appendChild(paragraph);
+    input.value = "";
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = 'line-through'
+    })
+    paragraph.addEventListener('dblclick',function(){
+        list.removeChild(paragraph);    
+    })
+})
